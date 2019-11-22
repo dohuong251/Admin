30002fecee86034caf20510be22636c2bc6db96a
chinh2597 <chinh2597@gmail.com>
2019-11-22 11:22:01 +0700

    fix controller name

--- routes/web.php
+++ routes/web.php
@@ -43,84 +43,84 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin'], function () {
         Route::group(['as' => 'lsp.', 'prefix' => 'livestreamplayer'], function () {
             // users
             Route::group(['as' => 'user.', 'prefix' => 'users'], function () {
-                Route::get('/', 'lsp\UserController@index')->name('index');
-                Route::get('/{userId}/streams', 'lsp\UserController@streams')->name('streams');
-                Route::get('/{userId}', 'lsp\UserController@show')->name('show');
+                Route::get('/', 'Lsp\UserController@index')->name('index');
+                Route::get('/{userId}/streams', 'Lsp\UserController@streams')->name('streams');
+                Route::get('/{userId}', 'Lsp\UserController@show')->name('show');
 
-                Route::put('/{userId}', 'lsp\UserController@update')->name('update');
+                Route::put('/{userId}', 'Lsp\UserController@update')->name('update');
 
-                Route::delete('/{userId}', 'lsp\UserController@destroy')->name('destroy');
-                Route::delete('/', 'lsp\UserController@delete')->name('delete');
+                Route::delete('/{userId}', 'Lsp\UserController@destroy')->name('destroy');
+                Route::delete('/', 'Lsp\UserController@delete')->name('delete');
             });
 
             // streams
             Route::group(['as' => 'streams.', 'prefix' => 'streams'], function () {
-                Route::get('/', 'lsp\StreamController@index')->name('index');
-                Route::get('/create', 'lsp\StreamController@create')->name('create');
-                Route::get('/dashboard', 'lsp\StreamController@filter')->name('dashboard');
-                Route::get('/complain', 'lsp\StreamController@complain')->name('complain');
-                Route::get('/features', 'lsp\StreamController@feature')->name('feature');
-                Route::get('/{songId}', 'lsp\StreamController@show')->name('show');
+                Route::get('/', 'Lsp\StreamController@index')->name('index');
+                Route::get('/create', 'Lsp\StreamController@create')->name('create');
+                Route::get('/dashboard', 'Lsp\StreamController@filter')->name('dashboard');
+                Route::get('/complain', 'Lsp\StreamController@complain')->name('complain');
+                Route::get('/features', 'Lsp\StreamController@feature')->name('feature');
+                Route::get('/{songId}', 'Lsp\StreamController@show')->name('show');
 
-                Route::post('/', 'lsp\StreamController@store')->name('store');
-                Route::post('/suspend', 'lsp\StreamController@suspend')->name('suspend');
+                Route::post('/', 'Lsp\StreamController@store')->name('store');
+                Route::post('/suspend', 'Lsp\StreamController@suspend')->name('suspend');
 
-                Route::put('/{songId}', 'lsp\StreamController@update')->name('update');
+                Route::put('/{songId}', 'Lsp\StreamController@update')->name('update');
 
-                Route::delete('/{songId}', 'lsp\StreamController@destroy')->name('destroy');
-                Route::delete('/', 'lsp\StreamController@delete')->name('delete');
+                Route::delete('/{songId}', 'Lsp\StreamController@destroy')->name('destroy');
+                Route::delete('/', 'Lsp\StreamController@delete')->name('delete');
             });
 
             // messages
             Route::group(['as' => 'messages.', 'prefix' => 'messages'], function () {
-                Route::get('/', 'lsp\MessageController@index')->name('index');
-                Route::post('/', 'lsp\MessageController@store')->name('store');
-                Route::delete('/', 'lsp\MessageController@delete')->name('delete');
+                Route::get('/', 'Lsp\MessageController@index')->name('index');
+                Route::post('/', 'Lsp\MessageController@store')->name('store');
+                Route::delete('/', 'Lsp\MessageController@delete')->name('delete');
             });
 
             Route::group(['as' => 'analytic.', 'prefix' => 'analytic'], function () {
-                Route::get('/statistics', 'lsp\StatisticsController@index')->name('statistics');
-                Route::get('/statistics/filter', 'lsp\StatisticsController@filter')->name('statistics.filter');
-                Route::get('/statistics/search', 'lsp\StatisticsController@search')->name('statistics.search');
-                Route::get('/realtime', 'lsp\RealtimeAnalyticController@index')->name('realtime');
+                Route::get('/statistics', 'Lsp\StatisticsController@index')->name('statistics');
+                Route::get('/statistics/filter', 'Lsp\StatisticsController@filter')->name('statistics.filter');
+                Route::get('/statistics/search', 'Lsp\StatisticsController@search')->name('statistics.search');
+                Route::get('/realtime', 'Lsp\RealtimeAnalyticController@index')->name('realtime');
             });
         });
 
 //        sales
         Route::group(['as' => 'sales.', 'prefix' => 'sales'], function () {
-            Route::get('/order', 'lsp\OrderController@index')->name('order');
-            Route::get('/subscription', 'lsp\SubscriptionController@index')->name('subscription');
-            Route::get('/license', 'lsp\LicenseController@index')->name('license');
+            Route::get('/order', 'Lsp\OrderController@index')->name('order');
+            Route::get('/subscription', 'Lsp\SubscriptionController@index')->name('subscription');
+            Route::get('/license', 'Lsp\LicenseController@index')->name('license');
         });
 
 //        tools
         Route::group(['as' => 'tools.', 'prefix' => 'tools'], function () {
-            Route::get('/config', 'tool\ConfigController@index')->name('config');
-            Route::post('/config', 'tool\ConfigController@store')->name('store');
-            Route::put('/config', 'tool\ConfigController@update')->name('update');
+            Route::get('/config', 'Tool\ConfigController@index')->name('config');
+            Route::post('/config', 'Tool\ConfigController@store')->name('store');
+            Route::put('/config', 'Tool\ConfigController@update')->name('update');
 
-            Route::get('/notification', 'tool\NotificationController@index')->name('notification');
-            Route::post('/notification', 'tool\NotificationController@store')->name('notification.store');
-            Route::get('/notification/create', 'tool\NotificationController@create')->name('notification.create');
-            Route::get('/notification/{notificationId}', 'tool\NotificationController@show')->name('notification.show');
-            Route::put('/notification/{notificationId}', 'tool\NotificationController@update')->name('notification.update');
-            Route::delete('/notification', 'tool\NotificationController@delete')->name('notification.delete');
+            Route::get('/notification', 'Tool\NotificationController@index')->name('notification');
+            Route::post('/notification', 'Tool\NotificationController@store')->name('notification.store');
+            Route::get('/notification/create', 'Tool\NotificationController@create')->name('notification.create');
+            Route::get('/notification/{notificationId}', 'Tool\NotificationController@show')->name('notification.show');
+            Route::put('/notification/{notificationId}', 'Tool\NotificationController@update')->name('notification.update');
+            Route::delete('/notification', 'Tool\NotificationController@delete')->name('notification.delete');
 
-            Route::get('/sendBroadcast', 'tool\SendBroadcastController@index')->name('sendBroadcast');
+            Route::get('/sendBroadcast', 'Tool\SendBroadcastController@index')->name('sendBroadcast');
 
-            Route::get('/testRule', 'tool\TestRuleController@index')->name('testRule');
-            Route::post('/testRule', 'tool\TestRuleController@decryptUrl')->name('decrypt_url');
-            Route::put('/testRule', 'tool\TestRuleController@updateRule')->name('testRule.update_rule');
+            Route::get('/testRule', 'Tool\TestRuleController@index')->name('testRule');
+            Route::post('/testRule', 'Tool\TestRuleController@decryptUrl')->name('decrypt_url');
+            Route::put('/testRule', 'Tool\TestRuleController@updateRule')->name('testRule.update_rule');
         });
 //        app
         Route::group(['as' => 'apps.', 'prefix' => 'apps'], function () {
-            Route::get('/', 'app\AppController@index')->name('index');
+            Route::get('/', 'App\AppController@index')->name('index');
 
-            Route::get('/{appId}', 'app\AppController@show')->name('show');
+            Route::get('/{appId}', 'App\AppController@show')->name('show');
         });
 
         Route::group(['as' => 'promotions.', 'prefix' => 'promotions'], function () {
-            Route::get('/', 'promotion\PromotionController@index')->name('index');
+            Route::get('/', 'Promotion\PromotionController@index')->name('index');
         });
     });
 
