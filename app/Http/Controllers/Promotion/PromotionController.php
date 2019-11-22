e195209592ef18cd7a86db4f71e3b21740f0f5d8
chinh2597 <chinh2597@gmail.com>
2019-11-22 11:10:27 +0700

    promotion index

--- app/Http/Controllers/Promotion/PromotionController.php
+++ app/Http/Controllers/Promotion/PromotionController.php
@@ -3,15 +3,26 @@
 namespace App\Http\Controllers\Promotion;
 
 use App\Http\Controllers\model;
+use DOMDocument;
+use DOMXPath;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
 class PromotionController extends Controller
 {
-    public function index(){
+    public function index()
+    {
+        $doc = new DOMDocument();
+        $doc->loadHTML('E:\MDCAdmin\Note\layout.php');
+        $finder = new DomXPath($doc);
+        $classname="footer";
+        $nodes = $finder->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' $classname ')]");
+        dd($nodes);
+        dd($doc->saveHTML());
         return view('promotions.index');
     }
     //
+
     /**
      * @return array các tham số share cho tất cả các view trả về bởi controller
      */
