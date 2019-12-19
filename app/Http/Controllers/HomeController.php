78bdbf961efc96063d1bce34f393a84836acf7eb
chinh2597 <chinh2597@gmail.com>
2019-12-19 11:46:24 +0700

    dashboard daily purchase chart

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -45,13 +45,6 @@ class HomeController extends Controller
         $startDate = $request->get('startDate');
         $endDate = $request->get('endDate');
 
-//        dd(LicenseKey::selectRaw('DATE(PurchaseDate) as PurchaseDate,count(*) as Total, 6 as PurchaseMethod')
-//            ->whereBetween('PurchaseDate', [$startDate, $endDate])
-//            ->whereIn('ApplicationId', ["Live Media Player", "Live Stream Player", "Live Stream Player AndroidTV"])
-//            ->groupBy(DB::raw('DATE(PurchaseDate)'))
-//            ->get());
-//        'SELECT COUNT(*) AS `Hàng`, `PurchaseMethod`, PurchaseDate FROM `orders` where PurchaseDate BETWEEN "2019-12-01" AND "2019-12-18" GROUP BY DATE(PurchaseDate), PurchaseMethod ORDER BY `PurchaseDate`'
-
         $lspOrders = Order::select(DB::raw('count(*) as Total, DATE(PurchaseDate) as PurchaseDate'))
             ->whereIn('ApplicationId', ["Live Media Player", "Live Stream Player", "Live Stream Player AndroidTV"])
             ->where('PurchaseMethod', "!=", 6)
@@ -70,34 +63,20 @@ class HomeController extends Controller
             })->map(function ($item, $key) {
                 return ["x" => $key, "y" => array_sum($item->toArray())];
             })->values()->all();
-//        foreach ($lspOrders as &$lspOrder) {
-//            if ($lspOrder["PurchaseMethod"] == 6) {
-//                //license key
-//                $lspOrder["Total"] = LicenseKey::selectRaw('DATE(PurchaseDate) as PurchaseDate,count(*) as Total, 6 as PurchaseMethod')
-//                    ->whereBetween('PurchaseDate', [$startDate, $endDate])
-//                    ->whereIn('ApplicationId', ["Live Media Player", "Live Stream Player", "Live Stream Player AndroidTV"])
-//                    ->groupBy(DB::raw('DATE(PurchaseDate)'))
-//                    ->get();
-//            }
-//        }
+
         return array(
-//            "LspStream" => array(
-//                "LastOnline" => Songs::whereBetween('LastOnline', [$startDate, $endDate])
-//                    ->selectRaw('LastOnline as Date, count(*) as Total')
-//                    ->groupBy('LastOnline')
-//                    ->orderBy('LastOnline', 'asc')
-//                    ->get(),
-//                "Published" => Songs::whereBetween('PublishedDate', [$startDate, $endDate])
-//                    ->selectRaw('PublishedDate as Date, count(*) as Total')
-//                    ->groupBy('PublishedDate')
-//                    ->orderBy('PublishedDate', 'asc')
-//                    ->get()),
+            "LspStream" => array(
+                "LastOnline" => Songs::whereBetween('LastOnline', [$startDate, $endDate])
+                    ->selectRaw('LastOnline as Date, count(*) as Total')
+                    ->groupBy('LastOnline')
+                    ->orderBy('LastOnline', 'asc')
+                    ->get(),
+                "Published" => Songs::whereBetween('PublishedDate', [$startDate, $endDate])
+                    ->selectRaw('PublishedDate as Date, count(*) as Total')
+                    ->groupBy('PublishedDate')
+                    ->orderBy('PublishedDate', 'asc')
+                    ->get()),
             "LspOrder" => $lspOrders,
-//            "USTVOrder" => Order::select('PurchaseMethod', DB::raw('count(*) as Total'))
-//                ->where('ApplicationId', "USTV")
-//                ->whereBetween('PurchaseDate', [$startDate, $endDate])
-//                ->groupBy('PurchaseMethod')
-//                ->get(),
             "USTVOrder" => Order::select(DB::raw('count(*) as Total, DATE(PurchaseDate) as PurchaseDate'))
                 ->where('ApplicationId', "USTV")
                 ->whereBetween('PurchaseDate', [$startDate, $endDate])
@@ -106,7 +85,6 @@ class HomeController extends Controller
                 ->get()
                 ->map(function ($item, $key) {
                     return ["x" => $item['PurchaseDate'], "y" => $item['Total']];
-//                    return [$item['PurchaseDate'] => $item['Total']];
                 }),
         );
     }
