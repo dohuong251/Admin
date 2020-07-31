b49d03d0c4bc95e23509efd623fb754999022355
chiennguyen <chiennq89@gmail.com>
2020-07-31 16:59:56 +0700

    overview

--- app/Http/Controllers/app/AppController.php
+++ app/Http/Controllers/app/AppController.php
@@ -56,11 +56,11 @@ class AppController extends Controller
             $query_clone = clone $query;
             $query_clone = $query_clone->whereBetween('Time',[$day.' 00:00:00',$day.' 23:59:59']);
             //return $query->toSql();
-            //$active_count[] = $query_clone->count();
-            $active_count[] = 0;
+            $active_count[] = $query_clone->get()->count();
+            //$active_count[] = 0;
             // new user
-            //$new_user_count[] = $query_clone->where('NewUser',1)->count();
-            $new_user_count[] = 0;
+            $new_user_count[] = $query_clone->where('NewUser',1)->get()->count();
+            //$new_user_count[] = 0;
         }
 
         // top country
@@ -84,7 +84,6 @@ class AppController extends Controller
 
         // top version
         $all_versions = $this->getVersions($app_id);
-        return $all_versions;
         return view('apps.overview', ['app_id'=>$app_id,
                                             'selected_version'=>$selected_version,
                                             'versions'=>$all_versions,
