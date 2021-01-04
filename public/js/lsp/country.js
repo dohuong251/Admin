82ce2ff4b9e8a7281498a24dfb4fc7d2737cbb84
chiennguyen <chiennq89@gmail.com>
2021-01-04 14:00:24 +0700

    country

--- public/js/lsp/country.js
+++ public/js/lsp/country.js
@@ -51,7 +51,17 @@ let startDate = moment(), endDate = moment(), userId, streamId, viewChart, viewC
         x: {
             format: "dd/MM"
         }
-    }
+    },
+    responsive: [{
+        breakpoint: 480,
+        options: {
+            legend: {
+                position: 'bottom',
+                offsetX: -10,
+                offsetY: 0
+            }
+        }
+    }]
 };
 
 $(document).ready(function () {
